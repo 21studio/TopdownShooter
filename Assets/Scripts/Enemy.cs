56d5c0f78a3d56b016f0681bcc3ad824f390d84a
Dmac <>
2019-05-17 15:11:34 +0900

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(UnityEngine.AI.NavMeshAgent))]
+public class Enemy : LivingEntity {
+
+    public enum State { Idle, Chasing, Attacking };
+    State currentState;
+
+    public ParticleSystem deathEffect;
+
+    UnityEngine.AI.NavMeshAgent pathfinder; // 자동변환된 코드, version?
+    Transform target;
+    LivingEntity targetEntity;
+    Material skinMaterial;
+
+    Color originalColor;
+
+    float attackDistanceThreshold = .5f;
+    float timeBetweenAttacks = 1;
+    float damage = 1;
+
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
+
+    bool hasTarget;
+
+    void Awake()
+    {
+        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        if (GameObject.FindGameObjectWithTag("Player") != null) {
+            hasTarget = true;
+
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            targetEntity = target.GetComponent<LivingEntity>();
+
+            myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+            targetCollisionRadius = GetComponent<CapsuleCollider>().radius;
+                     
+        }
+    }
+
+    protected override void Start () {
+        base.Start();
+
+        if (hasTarget) {
+			currentState = State.Chasing;
+            targetEntity.OnDeath += OnTargetDeath;			
+
+			StartCoroutine(UpdatePath());    
+        }
+
+	}
+
+    public void SetCharacteristics(float moveSpeed, int hitsToKillPlayer, float enemyHealth, Color skinColour) {
+        pathfinder.speed = moveSpeed;
+
+        if (hasTarget) {
+            damage = Mathf.Ceil(targetEntity.startingHealth / hitsToKillPlayer);
+        }
+        startingHealth = enemyHealth;
+
+        skinMaterial = GetComponent<Renderer>().material;
+        skinMaterial.color = skinColour;
+        originalColor = skinMaterial.color;
+    }
+
+    public override void TakeHit (float damage, Vector3 hitPoint, Vector3 hitDirection) {
+
+        if (damage >= health) {
+            Destroy(Instantiate(deathEffect.gameObject, hitPoint, Quaternion.FromToRotation(Vector3.forward, hitDirection)) as GameObject, deathEffect.startLifetime);
+        }
+        base.TakeHit(damage, hitPoint, hitDirection);
+    }
+
+    void OnTargetDeath() {
+        hasTarget = false;
+        currentState = State.Idle;
+    }
+	
+	void Update () {
+
+        if (hasTarget) {
+			if (Time.time > nextAttackTime)
+			{
+				float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+				if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+				{
+					nextAttackTime = Time.time + timeBetweenAttacks;
+					StartCoroutine(Attack());
+				}
+			}    
+        }
+	}
+
+    IEnumerator Attack() {
+
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+		Vector3 dirToTarget = (target.position - transform.position).normalized;
+		Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius);
+        // Vector3 attackPosition = target.position;
+
+        float attackSpeed = 3;
+        float percent = 0;
+
+        skinMaterial.color = Color.red;
+        bool hasAppliedDamage = false;
+
+        while (percent <= 1) {
+
+            if (percent > .5f && !hasAppliedDamage) {
+                hasAppliedDamage = true;
+                targetEntity.TakeDamage(damage);
+            }
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        skinMaterial.color = originalColor;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
+    }
+
+    IEnumerator UpdatePath() {
+        float refreshRate = 0.25f;
+
+        while (hasTarget) {
+            if (currentState == State.Chasing) {
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold/2);
+                //Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+
+				if (!dead) {
+					pathfinder.SetDestination(targetPosition);
+				}    
+            }
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerController))]
+[RequireComponent (typeof(GunController))]
+public class Player : LivingEntity {
+
+    public float moveSpeed = 5;
+
+    public Crosshairs crosshairs;
+
+    Camera viewCamera;
+    PlayerController controller;
+    GunController gunController;
+
+	protected override void Start () {
+        base.Start();
+
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+	}
+	
+	void Update () {
+
+        // Movement Input
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed; // normalized 는 방향을 가리키는 단위벡터로 만드는 연산이다
+        controller.Move(moveVelocity);
+
+        // Look Input 
+        Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.up * gunController.GunHeight);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance)) {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+            crosshairs.transform.position = point;
+            crosshairs.DectectTargets(ray);
+            if ((new Vector2(point.x, point.z) - new Vector2(transform.position.x, transform.position.z)).sqrMagnitude > 1) {
+                gunController.Aim(point);
+            }
+            //print((new Vector2(point.x, point.z) - new Vector2(transform.position.x, transform.position.z)).magnitude);
+        }
+
+        // Weapon Input
+        if (Input.GetMouseButton(0)) {
+            gunController.OnTriggerHold();
+        }
+		if (Input.GetMouseButtonUp(0)) {
+			gunController.OnTriggerRelease();
+		}
+        if (Input.GetKeyDown(KeyCode.R)) {
+            gunController.Reload();
+        }
+	}
+}
